a4cf8641807c96b41a6fefc2294a088ee70f335d
Ankit <kukuankitvyas@gmail.com>
2022-06-13 23:04:31 +0530

    updateValidation

--- api/users/users.js
+++ api/users/users.js
@@ -25,7 +25,7 @@ const addUser = (req, res, next) => {
             next(err)
         });
     } else {
-    next(details.error)
+        next(details.error)
     }
 
 }
@@ -46,27 +46,36 @@ const getUser = (req, res, next) => {
 }
 
 const updateUser = (req, res, next) => {
-    const data = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email
-    }
+    const schema = Joi.object({
 
-    models.User.update(data, {
-        where: {
-            id: req.params.id
+        firstName: Joi.string().min(2).required(),
+        lastName: Joi.string().min(2).required(),
+        email: Joi.string().email().min(2).required()
+    })
+    const details = schema.validate(req.body)
+    if (!details.error) {
+        const data = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email
         }
-    }).then((result) => {
-        res.json({
-            result
-        })
-    }).catch((err) => {
-        console.log(err)
-        res.json({
-            success: false,
-            message: 'Error occurred'
-        })
-    });
+
+        models.User.update(data, {
+            where: {
+                id: req.params.id
+            }
+        }).then((result) => {
+            res.json({
+                result
+            })
+        }).catch((err) => {
+            console.log(err)
+            res.json({
+                success: false,
+                message: 'Error occurred'
+            })
+        });
+    }
 }
 const deleteUser = (req, res, next) => {
     models.User.destroy({
